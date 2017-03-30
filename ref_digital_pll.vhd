855cd857bc560a8c2341c55e9988b4bfe6c9d03d
lharnaldi <lharnaldi@gmail.com>
2017-03-30 07:27:23 -0300

    Moved the pos/neg to up/down out from PD and put the up/dwn signals as input to the and gate for reset.

--- ref_digital_pll.vhd
+++ ref_digital_pll.vhd
@@ -9,12 +9,16 @@ port(
   dds_sync_i : in std_logic;
   ref_i      : in std_logic; -- reference signal after zero crossing
   dds_fbk_o  : out std_logic_vector (21 downto 0);
-  comp_pos_o : out std_logic;
-  comp_neg_o : out std_logic
+  comp_up_o  : out std_logic;
+  comp_dwn_o : out std_logic
 );
 end ref_digital_psd;
 
 architecture rtl of ref_digital_psd is
+
+constant gain: std_logic_vector(21 downto 0):="0010000000000000000000"; --16 Hz //524288
+signal comp_up_reg, comp_up_next : std_logic;
+signal comp_dwn_reg, comp_dwn_next : std_logic;
 
 signal dds_fbk_o_1: std_logic_vector(21 downto 0):=(others=>'0');
 signal pos_gain: std_logic_vector(21 downto 0):=(others=>'0');
@@ -24,32 +28,32 @@ signal neg_temp1: std_logic;
 signal pos_temp1: std_logic;
 signal out_pos_flash: std_logic;
 signal out_neg_flash: std_logic;
-constant gain: std_logic_vector(21 downto 0):="0010000000000000000000"; --16 Hz //524288
 
 begin
 
-comp_pos_o <= comp_pos_reg;
-comp_neg_o <= comp_neg_reg;
-reset      <= dds_sync_i and ref_i;
+comp_up_o <= comp_up_reg;
+comp_neg_o <= comp_dwn_reg;
+--reset      <= dds_sync_i and ref_i;
+reset      <= comp_up_reg and comp_dwn_reg;
 
 --phase comparator flip flops
 process(dds_sync_i)		
 begin
 if rising_edge(dds_sync_i) then
-  comp_neg_reg <= com_neg_next;
+  comp_dwn_reg <= com_neg_next;
 end if;
 end process;
 
-comp_neg_next <= '0' when (reset = '1') else '1';
+comp_dwn_next <= '0' when (reset = '1') else '1';
 
 process(ref_i)
 begin
 if rising_edge(ref_i)  then
-  comp_pos_reg <= com_pos_next;
+  comp_up_reg <= com_pos_next;
 end if;
 end process;
 
-comp_pos_next <= '0' when (reset = '1') else '1';
+comp_up_next <= '0' when (reset = '1') else '1';
 
 out_pos_flash <= pos_temp1;	
 out_neg_flash <= neg_temp1;
@@ -57,8 +61,8 @@ out_neg_flash <= neg_temp1;
 process(clk_i)
 begin
  if rising_edge(clk_i) then
-   pos_temp1<=comp_pos_reg;
-   neg_temp1<=comp_neg_reg;
+   pos_temp1<=comp_up_reg;
+   neg_temp1<=comp_dwn_reg;
 
 	 if ((out_neg_flash='1') and (out_pos_flash ='0')) then 
 	   dds_fbk_o_1(21 downto 0) <= std_logic_vector(unsigned(dds_fbk_o_1)-32); -- -1 hz step
