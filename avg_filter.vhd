ddd271264e960ddca50370dc7de891b2bc0940eb
lharnaldi <lharnaldi@gmail.com>
2017-04-02 14:00:47 -0300

    Change the TABs by 2 spaces. Nothing more.

--- avg_filter.vhd
+++ avg_filter.vhd
@@ -5,13 +5,13 @@ use ieee.numeric_std.all;
 
 entity avg_filter is
   port(
-		clk_i: in std_logic;
-		signal_i: in std_logic_vector(31 downto 0);
-		signal_o: out std_logic_vector(31 downto 0);
-		--SPARTAN_clk: in std_logic;
-		DDS_square_wave: in std_logic;
-		quantity: out std_logic_vector(23 downto 0);
-		sum: out std_logic_vector(33 downto 0)
+    clk_i: in std_logic;
+    signal_i: in std_logic_vector(31 downto 0);
+    signal_o: out std_logic_vector(31 downto 0);
+    --SPARTAN_clk: in std_logic;
+    DDS_square_wave: in std_logic;
+    quantity: out std_logic_vector(23 downto 0);
+    sum: out std_logic_vector(33 downto 0)
 );
 end avg_filter;
 
@@ -35,35 +35,35 @@ variable signal_acc_unsigned: unsigned (33 downto 0):=(others=>'0');
 variable signal_acc_shifted: unsigned (23 downto 0):=(others=>'0');
 begin
 if rising_edge(clk_i) then
-	previous_DDS_state <= DDS_square_wave;
+  previous_DDS_state <= DDS_square_wave;
 
-	if ((DDS_square_wave = '1') and (previous_DDS_state  = '0')) then			--if reference pulse goes high, begin count
+  if ((DDS_square_wave = '1') and (previous_DDS_state  = '0')) then      --if reference pulse goes high, begin count
 
-		if signal_acc(33)='1' then
-			--signal_acc_unsigned:=unsigned(std_logic_vector(signal_acc) xor all_ones_36)+1;
-			signal_acc_unsigned:=unsigned(-signal_acc);			
-		else
-			signal_acc_unsigned:=unsigned(signal_acc);
-		end if;
-		signal_acc_shifted:=resize(shift_right(signal_acc_unsigned, 9), 24);
-		
-		average_int:=signed("00000000" & divide(signal_acc_shifted, quantity_counter));
-		
-		if signal_acc(33)='1' then
-			average_int:=-average_int;	
-		else
-			average_int:=average_int;
-		end if;
-		average<=std_logic_vector(average_int);
-		quantity_counter <=((0)=>'1', others=>'0');
-		signal_acc<=(others=>'0');
-		sum<=std_logic_vector(signal_acc);
-		quantity<=std_logic_vector(quantity_counter);
-	else -- it counts for both positive and negative periods, but period is taken for one
-		quantity_counter <= quantity_counter + 1;
-		signal_acc<=signal_acc + signed(signal_i);
-	end if;
-	
+    if signal_acc(33)='1' then
+      --signal_acc_unsigned:=unsigned(std_logic_vector(signal_acc) xor all_ones_36)+1;
+      signal_acc_unsigned:=unsigned(-signal_acc);      
+    else
+      signal_acc_unsigned:=unsigned(signal_acc);
+    end if;
+    signal_acc_shifted:=resize(shift_right(signal_acc_unsigned, 9), 24);
+    
+    average_int:=signed("00000000" & divide(signal_acc_shifted, quantity_counter));
+    
+    if signal_acc(33)='1' then
+      average_int:=-average_int;  
+    else
+      average_int:=average_int;
+    end if;
+    average<=std_logic_vector(average_int);
+    quantity_counter <=((0)=>'1', others=>'0');
+    signal_acc<=(others=>'0');
+    sum<=std_logic_vector(signal_acc);
+    quantity<=std_logic_vector(quantity_counter);
+  else -- it counts for both positive and negative periods, but period is taken for one
+    quantity_counter <= quantity_counter + 1;
+    signal_acc<=signal_acc + signed(signal_i);
+  end if;
+  
 end if;
 end process;
 
--- pll.vhd
+++ pll.vhd
@@ -4,14 +4,14 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity pll is
-	port(
-		clk_i: in std_logic;
-		ref_in: in std_logic_vector (15 downto 0);
-		SPARTAN_clk: in std_logic;
-		DDS_square_out: out std_logic;
-		DDS_frequency: out std_logic_vector (23 downto 0);
-		refout_I: out std_logic_vector	(15 downto 0);
-		refout_Q: out std_logic_vector	(15 downto 0)
+  port(
+    clk_i: in std_logic;
+    ref_in: in std_logic_vector (15 downto 0);
+    SPARTAN_clk: in std_logic;
+    DDS_square_out: out std_logic;
+    DDS_frequency: out std_logic_vector (23 downto 0);
+    refout_I: out std_logic_vector  (15 downto 0);
+    refout_Q: out std_logic_vector  (15 downto 0)
 );
 end pll;
 
@@ -27,49 +27,49 @@ signal phase_Q: std_logic_vector (9 downto 0);
 
 begin
 
-	--ref_square_wave_out <= reference_square_wave;
-	DDS_square_out <= DDS_sync_output;
-	DDS_frequency <= DDS_feedback_out;
-	--ref_check <= reference_square_wave;
-	--DDS_check <= DDS_sync_output;
-	--freq <= DDS_feedback_out;
+  --ref_square_wave_out <= reference_square_wave;
+  DDS_square_out <= DDS_sync_output;
+  DDS_frequency <= DDS_feedback_out;
+  --ref_check <= reference_square_wave;
+  --DDS_check <= DDS_sync_output;
+  --freq <= DDS_feedback_out;
 
 SIGNAL_GENERATOR_1: entity work.signal_gen 
 port map (
-	frequency => DDS_feedback_out,	--output frequency
-	clk_in => SPARTAN_clk,				--
-	sync_output => DDS_sync_output,	--sync to PLL
-	phase_I => phase_I,		--output both I and Q
-	phase_Q => phase_Q
+  frequency => DDS_feedback_out,  --output frequency
+  clk_in => SPARTAN_clk,        --
+  sync_output => DDS_sync_output,  --sync to PLL
+  phase_I => phase_I,    --output both I and Q
+  phase_Q => phase_Q
 );
 
 phase_I_sine: entity work.sine_lut 
 port map (
-	THETA => phase_I,
-	CLK => SPARTAN_clk,
-	SINE_OUT => refout_I
+  THETA => phase_I,
+  CLK => SPARTAN_clk,
+  SINE_OUT => refout_I
 );
 
 phase_Q_cos: entity work.sine_lut 
 port map (
-	THETA => phase_Q,
-	CLK => SPARTAN_clk,
-	SINE_OUT => refout_Q
+  THETA => phase_Q,
+  CLK => SPARTAN_clk,
+  SINE_OUT => refout_Q
 );
 
 ZERO_CROSSING_DETECTOR_1: entity work.zero_crossing_det 
 port map (
-	sig_in => ref_in, 		--signal in should be in 2's comp. for zero crossing, we probably only need to look at sign bit. but for now, let's use full signal
-	signal_clock => clk_i,
-	crossing_detect_out => reference_square_wave
+  sig_in => ref_in,     --signal in should be in 2's comp. for zero crossing, we probably only need to look at sign bit. but for now, let's use full signal
+  signal_clock => clk_i,
+  crossing_detect_out => reference_square_wave
 );
 
 REF_FREQUENCY_1: entity work.ref_frequency 
 port map (
-	reference_sync_wave => reference_square_wave,
-	SPARTAN_clk => SPARTAN_clk,
-	clk_in => clk_i,
-	DDS_feedback_out => DDS_feedback_out
+  reference_sync_wave => reference_square_wave,
+  SPARTAN_clk => SPARTAN_clk,
+  clk_in => clk_i,
+  DDS_feedback_out => DDS_feedback_out
 );
 
 end rtl;
