3ae8a3a371e7a1170e6097f3c015b757ea679234
lharnaldi <lharnaldi@gmail.com>
2017-04-05 21:49:10 -0300

    Added the rst_i signal. The simulation is ok for this file.

--- zero_cross_det.vhd
+++ zero_cross_det.vhd
@@ -7,9 +7,8 @@ generic (
   ADC_DATA_WIDTH  : natural := 14
 );
 port (
-
-port (
   clk_i: in std_logic;
+  rst_i: in std_logic;
   sig_i: in std_logic_vector(ADC_DATA_WIDTH-1 downto 0); 		
   det_o: out std_logic
 );
@@ -23,32 +22,43 @@ constant hyst    : signed(ADC_DATA_WIDTH-1 downto 0) :="00100000000000";
 
 signal sig_i_reg, sig_i_next: std_logic_vector(ADC_DATA_WIDTH-1 downto 0) ;
 signal det_o_reg, det_o_next: std_logic;
-signal hyst_low  : std_logic;
-signal hyst_high : std_logic;
+signal hyst_low_reg, hyst_low_next  : std_logic;
+signal hyst_high_reg, hyst_high_next : std_logic;
 
 begin
 
 process(clk_i)
 begin
 if rising_edge(clk_i) then
-  sig_i_reg <= sig_i_next;
-  det_o_reg <= det_o_next;
+  if rst_i = '0' then
+    sig_i_reg <= (others => '0');
+    det_o_reg <= '0';
+    hyst_low_reg <= '0';
+    hyst_high_reg <= '0';
+  else  
+    sig_i_reg <= sig_i_next;
+    det_o_reg <= det_o_next;
+    hyst_low_reg <= hyst_low_next;
+    hyst_high_reg <= hyst_high_next;
+  end if;
 end if;
 end process;
 
 --Next state logic
 sig_i_next <= sig_i;
 
-det_o_next <= '1' when (sig_i_reg(sig_i_reg'left) = '1' and sig_i_next(sig_i_next'left) = '0' and hyst_low = '1') else
-              '0' when (sig_i_reg(sig_i_reg'left) = '0' and sig_i_next(sig_i_next'left) = '1' and hyst_high = '1') else
+det_o_next <= '1' when (sig_i_reg(sig_i_reg'left) = '1' and sig_i_next(sig_i_next'left) = '0' and hyst_low_reg = '1') else
+              '0' when (sig_i_reg(sig_i_reg'left) = '0' and sig_i_next(sig_i_next'left) = '1' and hyst_high_reg = '1') else
               det_o_reg;
 
-hyst_high <= '1' when (signed(sig_i_next) > (hyst)) else
-             '0' when (sig_i_reg(sig_i_reg'left) = '0' and sig_i_next(sig_i_next'left) = '1' and hyst_high = '1');
-              
-hyst_low  <= '1' when (signed(sig_i_next) < (-hyst)) else
-             '0' when (sig_i_reg(sig_i_reg'left) = '1' and sig_i_next(sig_i_next'left) = '0' and hyst_low = '1');
+hyst_low_next  <= '1' when (signed(sig_i_next) < (-hyst)) else
+                                '0' when (sig_i_reg(sig_i_reg'left) = '1' and sig_i_next(sig_i_next'left) = '0' and hyst_low_reg = '1') else
+                                hyst_low_reg;
 
+hyst_high_next <= '1' when (signed(sig_i_next) > (hyst)) else
+                  '0' when (sig_i_reg(sig_i_reg'left) = '0' and sig_i_next(sig_i_next'left) = '1' and hyst_high_reg = '1') else
+                  hyst_high_reg;
+              
 det_o <= det_o_reg;
 
 end rtl;
