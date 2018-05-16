6028521d3b1525383a09176bafd89b7a0d1d86c6
lharnaldi <lharnaldi@gmail.com>
2018-05-16 19:24:20 -0300

    Added the basic body for this core. Added the signed multiplier and the lpf cores.

--- lia.vhd
+++ lia.vhd
@@ -3,59 +3,88 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity top_adpll is
+entity lia is
 port(
   clk_i      : in std_logic;
+  rst_i      : in std_logic;
   sig_i      : in std_logic_vector(13 downto 0); 
   I          : out std_logic_vector(9 downto 0);
   Q          : out std_logic_vector(9 downto 0);
-  comp_up_o  : out std_logic;
-  comp_dn_o  : out std_logic;
   locked_o   : out std_logic
 );
-end top_adpll;
+end lia;
 
-architecture rtl of top_adpll is
+architecture rtl of lia is
 
-signal sig_zero_det_out : std_logic;
+signal zcd_o : std_logic;
 signal freq_sig : std_logic_vector(29 downto 0);
 signal sync_sig : std_logic;
 
+signal sin_sig, cos_sig : std_logic_vector(14-1 downto 0);
+
 begin
 
-U1: entity work.zero_cross_det
-generic map( ADC_DATA_WIDTH =>14)
+ZCD: entity work.zero_cross_det
+  generic map( ADC_DATA_WIDTH =>14)
+  port map(
+    clk_i => clk_i,
+    rst_i => rst_i,
+    sig_i => sig_i,
+    det_o => zcd_o
+  );
+
+ADPLL: entity work.adpll
+  port map(
+    clk_i      => clk_i,
+    rst_i      => rst_i,
+    ref_i      => zcd_o,
+  --  phase_I  => 
+  --  phase_Q  =>
+    locked_o   => locked_o,
+    sin_o      => sin_sig
+    cos_o      => cos_sig,
+  );
+
+SM1: entity work.signed_mult
+  generic map(
+  ADC_DATA_WIDTH => 14,
+  AXIS_TDATA_WIDTH => 32
+)
 port map(
   clk_i => clk_i,
-  sig_i => sig_i,
-  det_o => sig_zero_det_out
+  a_i   => sin_sig,
+  b_i   => sig_i,
+  mult_o => mult1_s
 );
 
-U2: entity work.adpll
+LPF1: entity work.exp_decay_filter
 port map(
-  clk_i      => clk_i,
-  ref_i      => sig_zero_det_out,
-  dds_sync_i => sync_sig,
-  dds_fbk_o  => freq_sig,
-  comp_up_o  => comp_up_o,
-  comp_dn_o  => comp_dn_o
+  aclk     => clk_i,
+  aresetn  => rst_i, 
+  sig_i    => mult1_s,
+  tc_i     => 
+  sig_o    => I
 );
 
-U3: entity work.ref_ddc_locker
+SM2: entity work.signed_mult
+  generic map(
+  ADC_DATA_WIDTH => 14,
+  AXIS_TDATA_WIDTH => 32
+)
 port map(
-  clk_i     => clk_i,
-  ref_i     => sig_zero_det_out,
-  dds_sync_i=> sync_sig,
-  locked_o  => locked_o
+  clk_i => clk_i,
+  a_i   => cos_sig,
+  b_i   => sig_i,
+  mult_o => mult2_s
 );
 
-U4: entity work.signal_gen
+LPF2: entity work.exp_decay_filter
 port map(
-  clk_i   => clk_i,
-  freq_i  => freq_sig,
-  sync_o  => sync_sig,
-  phase_I => I,
-  phase_Q => Q
+  aclk     => clk_i,
+  aresetn  => rst_i, 
+  sig_i    => mult2_s,
+  tc_i     => 
+  sig_o    => Q
 );
 
 end rtl;
