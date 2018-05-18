6417e609fae8870249f08c5f7f169e5542e6d017
lharnaldi <lharnaldi@gmail.com>
2018-05-18 11:10:30 -0300

    Change the width of the signal a and b and sign extended them.

--- signed_mult.vhd
+++ signed_mult.vhd
@@ -5,11 +5,12 @@ use ieee.numeric_std.all;
 
 entity signed_mult is
   generic (
+  ADC_DATA_WIDTH  : natural := 14;
   AXIS_TDATA_WIDTH: natural := 32
 );
 
 port(
-  clk_i : in std_logic;
+  aclk  : in std_logic;
   a_i   : in std_logic_vector (AXIS_TDATA_WIDTH/2-1 downto 0);
   b_i   : in std_logic_vector (AXIS_TDATA_WIDTH/2-1 downto 0);
   mult_o: out std_logic_vector (AXIS_TDATA_WIDTH-1 downto 0)
@@ -18,20 +19,22 @@ end signed_mult;
 
 architecture rtl of signed_mult is
 
-signal mult_reg, mult_next: std_logic_vector(AXIS_TDATA_WIDTH-1 downto 0) := (others => '0');
+ constant PADDING_WIDTH : natural := AXIS_TDATA_WIDTH/2 - ADC_DATA_WIDTH;
+signal mult_reg, mult_next: std_logic_vector(2*ADC_DATA_WIDTH-1 downto 0) := (others => '0');
 
 begin
 
-  process(clk_i)
+  process(aclk)
   begin
-    if rising_edge(clk_i) then
+    if rising_edge(aclk) then
       mult_reg <= mult_next;
     end if;
   end process;
 
   --Next state logic
-  mult_next <= std_logic_vector(signed(a_i) * signed(b_i));
+  mult_next <= std_logic_vector(signed(a_i(ADC_DATA_WIDTH-1 downto 0)) * signed(b_i(ADC_DATA_WIDTH-1 downto 0)));
 
-  mult_o <= mult_reg;
+  mult_o <= ((2*PADDING_WIDTH-1) downto 0 => mult_reg(2*ADC_DATA_WIDTH-1)) & mult_reg;
+  
 end rtl;
 
