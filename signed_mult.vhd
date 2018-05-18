cd71337e4b2d9999d97a3bfb1de8ff871846d76e
lharnaldi <lharnaldi@gmail.com>
2018-05-18 09:40:24 -0300

    Added the generic AXIS_TDATA_WIDTH to conform to the AXI standard.

--- signed_mult.vhd
+++ signed_mult.vhd
@@ -5,30 +5,29 @@ use ieee.numeric_std.all;
 
 entity signed_mult is
   generic (
-  ADC_DATA_WIDTH : natural := 14;
   AXIS_TDATA_WIDTH: natural := 32
 );
 
 port(
   clk_i : in std_logic;
-  a_i   : in std_logic_vector (ADC_DATA_WIDTH-1 downto 0);
-  b_i   : in std_logic_vector (ADC_DATA_WIDTH-1 downto 0);
-  mult_o: out std_logic_vector (2*ADC_DATA_WIDTH-1 downto 0)
+  a_i   : in std_logic_vector (AXIS_TDATA_WIDTH/2-1 downto 0);
+  b_i   : in std_logic_vector (AXIS_TDATA_WIDTH/2-1 downto 0);
+  mult_o: out std_logic_vector (AXIS_TDATA_WIDTH-1 downto 0)
 );
 end signed_mult;
 
 architecture rtl of signed_mult is
 
-signal mult_reg, mult_next: std_logic_vector(2*ADC_DATA_WIDTH-1 downto 0) := (others=>'0');
+signal mult_reg, mult_next: std_logic_vector(AXIS_TDATA_WIDTH-1 downto 0) := (others => '0');
 
 begin
 
-process (clk_i)
-begin
-  if rising_edge(clk_i) then
-    mult_reg <= mult_next;
-  end if;
-end process;
+  process(clk_i)
+  begin
+    if rising_edge(clk_i) then
+      mult_reg <= mult_next;
+    end if;
+  end process;
 
   --Next state logic
   mult_next <= std_logic_vector(signed(a_i) * signed(b_i));
